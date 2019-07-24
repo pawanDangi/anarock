d8a857e232772df244ccebbcbceb50eaf54ab83a
Pawan Dangi <pdangi@intertrust.com>
2019-07-25 00:47:08 +0530

    chnaged table design

--- src/components/TableView.jsx
+++ src/components/TableView.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { withStyles, makeStyles } from '@material-ui/core/styles';
+import { withStyles, makeStyles, useTheme } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import TableFooter from '@material-ui/core/TableFooter';
+import TablePagination from '@material-ui/core/TablePagination';
+import IconButton from '@material-ui/core/IconButton';
+import FirstPageIcon from '@material-ui/icons/FirstPage';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import LastPageIcon from '@material-ui/icons/LastPage';
 import moment from 'moment';
 
 const StyledTableCell = withStyles(theme => ({
@@ -41,20 +48,106 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const getKeys = (data = {}) =>
-  Object.keys(data)
+const useStyles1 = makeStyles(theme => ({
+  root: {
+    flexShrink: 0,
+    color: theme.palette.text.secondary,
+    marginLeft: theme.spacing(2.5)
+  }
+}));
+
+function TablePaginationActions(props) {
+  const classes = useStyles1();
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onChangePage } = props;
+
+  function handleFirstPageButtonClick(event) {
+    onChangePage(event, 0);
+  }
+
+  function handleBackButtonClick(event) {
+    onChangePage(event, page - 1);
+  }
+
+  function handleNextButtonClick(event) {
+    onChangePage(event, page + 1);
+  }
+
+  function handleLastPageButtonClick(event) {
+    onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  }
+
+  return (
+    <div className={classes.root}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="First Page"
+      >
+        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="Previous Page"
+      >
+        {theme.direction === 'rtl' ? (
+          <KeyboardArrowRight />
+        ) : (
+          <KeyboardArrowLeft />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="Next Page"
+      >
+        {theme.direction === 'rtl' ? (
+          <KeyboardArrowLeft />
+        ) : (
+          <KeyboardArrowRight />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="Last Page"
+      >
+        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </div>
+  );
+}
+
+const getKeys = (rows = {}) =>
+  Object.keys(rows)
     .map(k => k.split('_').map(a => a.charAt(0).toUpperCase() + a.slice(1)))
     .map(k => k.join(' '));
 
-export default function CustomizedTables({ data }) {
+export default function TableView({ data: rows }) {
   const classes = useStyles();
 
-  const keys = getKeys(data[0]);
-  const index = Object.keys(data[0] || {});
+  const keys = getKeys(rows[0]);
+  const index = Object.keys(rows[0] || {});
+
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const emptyRows =
+    rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+
+  function handleChangePage(event, newPage) {
+    setPage(newPage);
+  }
+
+  function handleChangeRowsPerPage(event) {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  }
 
   return (
     <Paper className={classes.root}>
-      {data && data.length ? (
+      {rows && rows.length ? (
         <Table className={classes.table}>
           <TableHead>
             <TableRow>
@@ -64,18 +157,44 @@ export default function CustomizedTables({ data }) {
             </TableRow>
           </TableHead>
           <TableBody>
-            {data.map((row, i) => (
-              <StyledTableRow key={`${i}-${row.id}`}>
-                {(index || []).map(k => (
-                  <StyledTableCell key={k}>
-                    {['updated_at', 'created_at'].indexOf(k) > -1
-                      ? moment(row[k]).format('MM/DD/YYYY')
-                      : row[k]}
-                  </StyledTableCell>
-                ))}
-              </StyledTableRow>
-            ))}
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row, i) => (
+                <StyledTableRow key={`${i}-${row.id}`}>
+                  {(index || []).map(k => (
+                    <StyledTableCell key={k}>
+                      {['updated_at', 'created_at'].indexOf(k) > -1
+                        ? moment(row[k]).format('MM/DD/YYYY')
+                        : row[k]}
+                    </StyledTableCell>
+                  ))}
+                </StyledTableRow>
+              ))}
+
+            {emptyRows > 0 && (
+              <TableRow style={{ height: 48 * emptyRows }}>
+                <TableCell colSpan={(keys || []).length} />
+              </TableRow>
+            )}
           </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                rowsPerPageOptions={[5, 10, 25]}
+                colSpan={3}
+                count={rows.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                SelectProps={{
+                  inputProps: { 'aria-label': 'Rows per page' },
+                  native: true
+                }}
+                onChangePage={handleChangePage}
+                onChangeRowsPerPage={handleChangeRowsPerPage}
+                ActionsComponent={TablePaginationActions}
+              />
+            </TableRow>
+          </TableFooter>
         </Table>
       ) : (
         <div className={classes.noData}>No Data Found</div>
